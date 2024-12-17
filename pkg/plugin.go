9821183e105d52e1606ca2ea53000476ee432be6
Mark Lehrer <mark@tpsit.com>
2024-12-17 15:10:32 +0000

    Added option to reconfigure gRPC buffer sizes

--- pkg/plugin.go
+++ pkg/plugin.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
 	"net/http"
 
 	"github.com/alexanderzobnin/grafana-zabbix/pkg/datasource"
@@ -22,10 +25,21 @@ func main() {
 
 	pluginLogger.Debug("Starting Zabbix datasource")
 
+	//
+	//  Read the buffer sizes from grafana.ini plugin section
+	//
+	//  Should be validated to be between 1 and 1000, default 16
+	//
+	maxSendSize, maxReceiveSize := LookupGRPCSizes(pluginLogger)
+
 	err := backend.Manage(ZABBIX_PLUGIN_ID, backend.ServeOpts{
 		CallResourceHandler: httpResourceHandler,
 		QueryDataHandler:    ds,
 		CheckHealthHandler:  ds,
+		GRPCSettings: backend.GRPCSettings {
+			MaxSendMsgSize: 1024 * 1024 * maxSendSize,
+			MaxReceiveMsgSize: 1024 * 1024 * maxReceiveSize,
+		},
 	})
 	if err != nil {
 		pluginLogger.Error("Error starting Zabbix datasource", "error", err.Error())
@@ -41,3 +55,39 @@ func Init(logger log.Logger, mux *http.ServeMux) *datasource.ZabbixDatasource {
 
 	return ds
 }
+
+func LookupGRPCSizes(logger log.Logger) (int, int) {
+	//
+	//  Read the buffer sizes from grafana.ini plugin section, in megabytes
+	//
+	//  Use 16 if undefined or out of range
+	//
+	//  Attempting to do proper input validation.
+	//
+	var maxSendSize int
+	var maxReceiveSize int
+
+	maxSendSize = 16
+	maxReceiveSize = 16
+
+        iniMaxSendSize := os.Getenv( "GF_PLUGIN_GRPC_MAX_SEND_MESSAGE_SIZE" )
+        iniMaxReceiveSize := os.Getenv( "GF_PLUGIN_GRPC_MAX_RECEIVE_MESSAGE_SIZE" )
+
+        //  Validate that they are integers between 1 and 1000, use 16 otherwise
+	if v, err := strconv.Atoi(iniMaxSendSize); err == nil {
+		if ( v >= 1 && v <= 1000 ) {
+			maxSendSize = v
+		}
+	}
+
+	if v, err := strconv.Atoi(iniMaxReceiveSize); err == nil {
+		if ( v >= 1 && v <= 1000 ) {
+			maxReceiveSize = v
+		}
+	}
+
+	logger.Info( fmt.Sprintf( "Setting gRPC max sizes: send %d and receive %d", maxSendSize, maxReceiveSize ) )
+
+	return maxSendSize, maxReceiveSize
+}
+
